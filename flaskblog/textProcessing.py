1aea9e55eb79a9ed92d8adbf6aa66a613c14a4f7
agataszary <agszary@gmail.com>
2021-05-04 18:03:00 +0200

    zarys klasy do przetwarzania tekstu

--- flaskblog/textProcessing.py
+++ flaskblog/textProcessing.py
@@ -1,8 +1,51 @@
 import pdfplumber
 import nltk
+import os
 from nltk.tokenize.punkt import PunktParameters, PunktSentenceTokenizer
 from nltk.corpus import gutenberg
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktTrainer
+
+
+class TextProcessing:
+    txt_filename = ""
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def pdf_to_txt(self):
+        with pdfplumber.open(self.filename) as pdf_text:
+            txt_filename = os.path.basename(self.filename)
+            txt_filename = os.path.splitext(txt_filename)[0]
+            txt_filename = os.path.join(txt_filename, ".txt")
+            self.txt_filename = txt_filename
+            txt_text = open(txt_filename, "w")
+            for i in range(0, len(pdf.pages)):
+                page = pdf_text.pages[i]
+                txt_text.writelines(page.extract_text())
+            txt_text.close()
+
+    def split_sentences(self, n):
+        file_to_read = open(self.txt_filename, "r")
+        file_to_read = file_to_read.read()
+        detector = nltk.data.load('tokenizers/punkt/english.pickle')
+        detector._params.abbrev_types.add('e.g')
+        tokens = detector.tokenize(file_to_read)
+        ind = 0
+        current_sentences = ""
+        sentences = []
+        for t in tokens:
+            current_sentences = current_sentences + " " + t
+            if ind < n:
+                ind += 1
+            else:
+                ind = 0
+                sentences.append(current_sentences)
+                current_sentences = ""
+        file_to_read.close()
+
+        return sentences
+
+
 
 with pdfplumber.open("static\\test.pdf") as pdf:
     txt_file = open("static\\test.txt", "w")
@@ -24,11 +67,11 @@ print("*************")
 # tokens = nltk.sent_tokenize(read_file)
 # print(type(tokens))
 
-sentences = "Mr. James told me Dr. Brown is not available today. I will e.g. try tomorrow S. Gray."
+sentence = "Mr. James told me Dr. Brown is not available today. I will e.g. try tomorrow S. Gray."
 
 sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
 sent_detector._params.abbrev_types.add('e.g')
-tokens = sent_detector.tokenize(read_file)
+tokens_list = sent_detector.tokenize(read_file)
 
 # text = ""
 # for file_id in gutenberg.fileids():
@@ -47,7 +90,7 @@ tokens = sent_detector.tokenize(read_file)
 
 index = 0
 current = ""
-for t in tokens:
+for t in tokens_list:
     # print(t + "\n")
     current = current + " " + t
     #na razie co 6 zdań
